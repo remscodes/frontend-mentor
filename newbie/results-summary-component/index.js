3b37dfb08783580e21be854d29f5d03296acc864
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2023-11-03 17:18:37 +0100

    [results-summary-component] Remove some web components

--- newbie/results-summary-component/index.js
+++ newbie/results-summary-component/index.js
@@ -1,3 +1,18 @@
 import { registerComponents } from './components/register-components';
+import summariesData from './data.json';
+
+function renderSummaries() {
+  const slot = document.getElementById('summaries-slot');
+
+  summariesData.forEach((summaryData) => {
+    const staticSummary = document.createElement('app-summary');
+    staticSummary.setAttribute('category', summaryData.category)
+    staticSummary.setAttribute('score', summaryData.score)
+    staticSummary.setAttribute('icon', summaryData.icon)
+
+    slot.appendChild(staticSummary);
+  });
+}
 
 registerComponents();
+renderSummaries();
